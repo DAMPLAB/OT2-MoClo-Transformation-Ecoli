85aa937a770f17030338809e5da9aabe35899ab7
Rita Chen <richen@bu.edu>
2020-11-18 01:55:10 -0500

    Update moclo_transform_template.py

--- moclo_transformation/data/moclo_transform_template.py
+++ moclo_transformation/data/moclo_transform_template.py
@@ -11,23 +11,8 @@ import math
 
 from opentrons import robot, instruments, labware, modules
 
-################################################################################
-'''
-COLD_BLOCK = '96-PCR-tall-cold-block'
-try:
-	labware.create(
-				   COLD_BLOCK,
-				   grid=(12, 8),
-				   spacing=(9, 9),
-				   diameter=5,
-				   depth=15.4,
-				   volume=200)
-except:
-	print("Using existing labware definition for {0}".format(COLD_BLOCK))
-'''
-################################################################################
-
 num_rxns = len(combinations_to_make)
+num_cols = math.ceil(num_rxns/8.0)
 #num_plates = math.ceil(num_rxns/24) #Amount of agar plates need for plating the transformed cells
 
 ################################################################################
@@ -39,21 +24,19 @@ temp_deck = modules.load('tempdeck', '10')
 reaction_plate = labware.load('biorad_96_wellplate_200ul_pcr', '10', share=True)
 temp_deck.set_temperature(10)
 
+# Need to provide the instructions for preparing 2x Master Mix for MoClo assembly
+MM_2_part_1 = reaction_plate.wells('A12')
+MM_2_part_2 = reaction_plate.wells('B12')
+MM_5_part_1 = reaction_plate.wells('C12')
+MM_8_part_1 = reaction_plate.wells('D12')
+
 # Load in 1 10ul tiprack and 2 300ul tipracks
 tr_10 = [labware.load('opentrons_96_tiprack_10ul', '3')]
 tr_300 = [labware.load('opentrons_96_tiprack_300ul', '6'), labware.load('opentrons_96_tiprack_300ul', '9')]
-#for i in range(0, 1):
-#   tr_300.append(labware.load('tipone_96_tiprack_200ul', '9'))
 
 # Load in pipettes
 p10_single = instruments.P10_Single(mount='right', tip_racks=tr_10)
-p300_multi = instruments.P300_Multi(mount='left', tip_racks=tr_300)
-
-# Need to provide the instructions for loading reagent
-reagents_plate = labware.load('biorad_96_wellplate_200ul_pcr', '4', 'Reagents Plate')
-ligase = reagents_plate.wells('H12') #MoClo
-restriction_enzyme = reagents_plate.wells('G12') #MoClo
-buffer = reagents_plate.wells('F12') # MoClo
+p300_multi = instruments.P300_Multi_Gen2(mount='left', tip_racks=tr_300)
 
 ################################################################################
 # This deck slot location is dedicated for the reaction plate after MoClo protocol is completed, so at the beginning of the protocol there isn't an actual plate existing in this slot location.
@@ -63,11 +46,10 @@ post_moclo_reaction_plate = labware.load('biorad_96_wellplate_200ul_pcr', '7', '
 
 # Load in water, SOC, and wash trough (USA Scientific 12 Well Reservoir 22ml)
 trough = labware.load('usascientific_12_reservoir_22ml', '5', 'Reagents trough')
-water = trough.wells(0) #Well 1
-wash_0 = trough.wells(1) #Well 2
-wash_1 = trough.wells(2) #Well 3
+wash_0 = trough.wells(0) #Well 1
+wash_1 = trough.wells(1) #Well 2
 soc = trough.wells(3) #Well 4
-liquid_waste = trough.wells(4) #Well 5
+liquid_waste = trough.wells(5) #Well 6
 
 # Load in up to 2 DNA plates (Bio-Rad 96 Well Plate 200ul PCR)
 #plate_name = dna_plate_map_dict.keys() # because there should be only 1 input plate
@@ -87,7 +69,7 @@ agar_plate = labware.load('e-gelgol', '2', 'Agar Plate')
 ################################################################################
 # For this protocol, the biorad_96_wellplate_200ul_pcr, is placed on the top of the TempDeck alone without the metal part.
 ################################################################################
-
+'''
 # Add water, buffer, restriction enzyme, ligase, and buffer to 2x master mix (2xMM).
 #Prepare 2xMM for 2-part assembly at 20uL total volume
 for i in range(2):
@@ -195,37 +177,68 @@ for i in range(12):
 	p10_single.blow_out()
 p10_single.drop_tip()
 
-'''
-	# Add 2xMM to 2-part assembly for varies volumes (5,10,20 uL)
-	count = 0
-	for i in range(3):
+# Add 2xMM to 2-part assembly for varies volumes (5,10,20 uL)
+count = 0
+for i in range(3):
 	for j in range(8):
-	p10_single.pick_up_tip()
-	p10_single.transfer(4*(2**i), reaction_plate.wells(89+(3*i)).bottom(0.3), reaction_plate.wells(3*j+count).bottom(0.3), new_tip='never')
-	p10_single.blow_out()
-	p10_single.drop_tip()
-	count += 1
+		p10_single.pick_up_tip()
+		p10_single.transfer(4*(2**i), reaction_plate.wells(89+(3*i)).bottom(0.3), reaction_plate.wells(3*j+count).bottom(0.3), new_tip='never')
+		p10_single.blow_out()
+		p10_single.drop_tip()
+		count += 1
 
-	# Add 2xMM to 5-part assembly for varies volumes (5,10,20 uL)
-	count = 0
-	for i in range(3):
+# Add 2xMM to 5-part assembly for varies volumes (5,10,20 uL)
+count = 0
+for i in range(3):
 	for j in range(8):
-	p10_single.pick_up_tip()
-	p10_single.transfer(2.5*(2**i), reaction_plate.wells(88+(3*i)).bottom(0.3), reaction_plate.wells(27+3*j+count).bottom(0.3), new_tip='never')
-	p10_single.blow_out()
-	p10_single.drop_tip()
-	count += 1
+	 p10_single.pick_up_tip()
+	 p10_single.transfer(2.5*(2**i), reaction_plate.wells(88+(3*i)).bottom(0.3), reaction_plate.wells(27+3*j+count).bottom(0.3), new_tip='never')
+	 p10_single.blow_out()
+	 p10_single.drop_tip()
+	 count += 1
 
-	# Add 2xMM to 8-part assembly for varies volumes (5,10,20 uL)
-	count = 0
-	for i in range(3):
+# Add 2xMM to 8-part assembly for varies volumes (5,10,20 uL)
+count = 0
+for i in range(3):
 	for j in range(8):
-	p10_single.pick_up_tip()
-	p10_single.transfer(1*(2**i), reaction_plate.wells(87+(3*i)).bottom(0.3), reaction_plate.wells(54+3*j+count).bottom(0.3), new_tip='never')
-	p10_single.blow_out()
-	p10_single.drop_tip()
-	count += 1
+	 p10_single.pick_up_tip()
+	 p10_single.transfer(1*(2**i), reaction_plate.wells(87+(3*i)).bottom(0.3), reaction_plate.wells(54+3*j+count).bottom(0.3), new_tip='never')
+	 p10_single.blow_out()
+	 p10_single.drop_tip()
+	 count += 1
 '''
+
+# Add master mix to each wells that would contain rxn
+# Add 2xMM to 2-part assembly
+p10_single.pick_up_tip()
+for i in range(6):
+	p10_single.mix(2, 10, MM_2_part_1.bottom())
+	p10_single.transfer(16, MM_2_part_1.bottom(), reaction_plate.wells(i).bottom(0.3), new_tip='never')
+	p10_single.blow_out()
+p10_single.drop_tip()
+
+p10_single.pick_up_tip()
+for i in range(6):
+	p10_single.mix(2, 10, MM_2_part_2.bottom())
+	p10_single.transfer(16, MM_2_part_2.bottom(), reaction_plate.wells(6+i).bottom(0.3), new_tip='never')
+	p10_single.blow_out()
+p10_single.drop_tip()
+
+# Add 2xMM to 5-part assembly
+p10_single.pick_up_tip()
+for i in range(12):
+	p10_single.mix(2, 10, MM_5_part_1.bottom())
+	p10_single.transfer(10, MM_5_part_1.bottom(), reaction_plate.wells(12+i).bottom(0.3), new_tip='never')
+	p10_single.blow_out()
+p10_single.drop_tip()
+
+# Add 2xMM to 8-part assembly
+p10_single.pick_up_tip()
+for i in range(12):
+	p10_single.mix(2, 10, MM_8_part_1.bottom())
+	p10_single.transfer(4, MM_8_part_1.bottom(), reaction_plate.wells(24+i).bottom(0.3), new_tip='never')
+	p10_single.blow_out()
+p10_single.drop_tip()
 
 #This function checks the existance of DNA parts and returns for well location of the parts
 def find_dna(name, dna_plate_map_dict, dna_plate_dict):
@@ -278,74 +291,60 @@ for part, combinations in combinations_by_part.items():
 	# Two washing steps are added to allow recycling of the tips
 	p10_single.drop_tip()
 
-# Seal the Reaction Plate with adhesive film, and incubate plate for 35 cycles of 37C for 1.5 minutes and 16C for 3 minutes, followed by 1 cycle at 50C for 5 minutes, and 80C for 10 minutes.
+robot.pause()
 
 ################################################################################
-# Seal the Reaction Plate to avoid liquid evaporation.
-# Discard the empty PCR tubes previously containing buffer, ligase, and restriction enzyme.
+# Seal the Reaction Plate with adhesive film, and incubate plate for 35 cycles of 37C for 1.5 minutes and 16C for 3 minutes, followed by 1 cycle at 50C for 5 minutes, and 80C for 10 minutes.
+
 # Remove the Input_DNA_Plate from the Deck Space. Remaining DNA may be saved by sealing the Input_DNA_Plate with adhesive film and storing at -20°C
 ################################################################################
 
-robot.pause()
-
 #####################Start the Transformation protocol##########################
+
 ################################################################################
 # The robotic liquid handler would automatically pause when the modular cloning protocol is completed, indicated by the blue light (cooling down) of the Temperature Module.
 
-#Remove the reaction_plate from the top of the Temperature Module and place it on Slot 7 which is assigned to the post_moclo_reaction_plate at the beginning of the protocol, and remove the adhesive film.
+#Place the reaction_plate, after thermocycling, on Slot 7 which is assigned to the post_moclo_reaction_plate at the beginning of the protocol, and remove the adhesive film.
 
-#Before beginning the Cell Transformation protocol, load 10 μL/well of chemically competent cells into a new Bio-Rad 96 Well Plate and place it on the top of the Temperature Module.
+#Before beginning the Cell Transformation protocol, load 15 μL/well of chemically competent cells into a new Bio-Rad 96 Well Plate and place it on the top of the Temperature Module without the Opentrons 96 well aluminum block.
 
 #Be sure to un-pause the robot after completing all the steps listed above!
 ################################################################################
 
-#for i in range(0, num_cols):
-#Using letters for rows of custom container to maintain backwards compatibility.
-#p300_multi.aspirate(10, reagents_plate.wells('A' + str(i + 1)).bottom(0.5))
-#p300_multi.dispense(10, reaction_plate.wells(48 + i*8).bottom(0.5))
-#p300_multi.drop_tip()
+'''Use 15uL of competent cells with 1.5uL of each DNA rections from the MoClo protocol'''
 
-'''Use 10uL of competent cells with 2uL of each DNA rections from the MoClo protocol'''
+# At the beginning of this for loop, the Bio-Rad 96 Well Plate (reaction_plate) now contains 15 uL of competent cells per reaction wells (well 1 through 88)
 
-# At the beginning of this for loop, the Bio-Rad 96 Well Plate (reaction_plate) now contains 10 uL of competent cells per wells (well 1 through 88)
-p10_single.pick_up_tip()
-# Add 2 ul of rxns to comp cells
+temp_deck.set_temperature(4)
+
+# Add 1.5 ul of MoClo rxns to comp cells
 for i in range(0, num_rxns):
-	p10_single.transfer(2, post_moclo_reaction_plate.wells(i).bottom(0.5), reaction_plate.wells(i).bottom(0.5), new_tip='never')
+	p10_single.pick_up_tip()
+	p10_single.transfer(1.5, post_moclo_reaction_plate.wells(i).bottom(0.5), reaction_plate.wells(i).bottom(0.5), new_tip='never')
 	p10_single.mix(4, 10, reaction_plate.wells(i).bottom(0.5))
 	p10_single.blow_out()
-	p10_single.mix(2, 10, wash_0.bottom(0.5))
-	p10_single.blow_out()
-	p10_single.mix(2, 10, wash_1.bottom(0.5))
-	p10_single.blow_out()
-# Two washing steps are added to allow recycling of the tips
-p10_single.drop_tip()
+	p10_single.drop_tip()
 
 # Incubate at 4C, then heat shock.
-'''Be sure to un-pause the robot in between the heat shock'''
-temp_deck.set_temperature(4)
 p10_single.delay(minutes=30)
 temp_deck.set_temperature(42)
-p10_single.delay(minutes=1)
+p10_single.delay(seconds=60)
 temp_deck.set_temperature(4)
-p10_single.delay(minutes=5)
+p10_single.delay(minutes=6)
 
 # Add soc.
-p300_multi.pick_up_tip()
 for i in range(0, num_cols):
-	p300_multi.transfer(150, soc.bottom(), reaction_plate.cols(i).bottom(1), new_tip='never')
+	p300_multi.pick_up_tip()
+	p300_multi.transfer(180, soc.bottom(), reaction_plate.cols(i).bottom(1), new_tip='never')
 	p300_multi.mix(2, 150, reaction_plate.cols(i).bottom(1))
 	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_0.bottom())
-	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_1.bottom())
-	p300_multi.blow_out()
-# Two washing steps are added to allow recycling of the tips
-p300_multi.drop_tip()
+	p300_multi.drop_tip()
 
-# Grow for 1 hr, seal the plate with adhesive film to avoid evaporation
+################################################################################
+# Incubate @37C for 1 hr, seal the plate with adhesive film to avoid evaporation
+################################################################################
 temp_deck.set_temperature(37)
-p10_single.delay(minutes=60)
+p10_single.delay(minutes=65)
 robot.pause()
 temp_deck.deactivate()
 
@@ -354,30 +353,21 @@ temp_deck.deactivate()
 # Be sure to un-pause the robot after removing the adhesive film!
 ################################################################################
 
+
 # Dilute the recovered transformation reactions and start plating
 ################################################################################
 # All recovered transformation reactions are diluted to 10% of its original concentration before plating
 ################################################################################
-
 #Dilution
-p300_multi.pick_up_tip()
 for i in range(0, num_cols):
-	p300_multi.transfer(157, reaction_plate.cols(i).bottom(0.5), liquid_waste.bottom(), new_tip='never')
+	p300_multi.pick_up_tip()
+	p300_multi.mix(2, 180, reaction_plate.cols(i).bottom(0.5))
+	p300_multi.transfer(191.5, reaction_plate.cols(i).bottom(0.5), liquid_waste.bottom())
 	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_0.bottom())
-	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_1.bottom())
-	p300_multi.blow_out()
-	# Two washing steps are added to allow recycling of the tips
 	p300_multi.transfer(45, soc.bottom(), reaction_plate.cols(i).bottom(0.5), new_tip='never')
-	p300_multi.mix(2, 50, reaction_plate.cols(i).bottom(0.5))
+	p300_multi.mix(2, 40, reaction_plate.cols(i).bottom(0.5))
 	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_0.bottom())
-	p300_multi.blow_out()
-	p300_multi.mix(2, 300, wash_1.bottom())
-	p300_multi.blow_out()
-# Two washing steps are added to allow recycling of the tips
-p300_multi.drop_tip()
+	p300_multi.drop_tip()
 
 #Plating
 ################################################################################
@@ -386,13 +376,13 @@ p300_multi.drop_tip()
 #count2 = 0
 for i in range(0, num_cols):
 	p300_multi.pick_up_tip()
-	p300_multi.transfer(10, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i).bottom(0.3), new_tip='never')
-	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i).bottom(-1), new_tip='never')
-	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2).bottom(0.8), new_tip='never')
-	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2).bottom(-1), new_tip='never')
-	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+1).bottom(0.8), new_tip='never')
-	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+1).bottom(-1), new_tip='never')
-	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+2).bottom(0.8), new_tip='never')
-	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+2).bottom(-1), new_tip='never')
+	p300_multi.transfer(10, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i).bottom(0.3))
+	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i).bottom(-1))
+	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2).bottom(0.8))
+	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2).bottom(-1))
+	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+1).bottom(0.8))
+	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+1).bottom(-1))
+	#p300_multi.transfer(9, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+2).bottom(0.8))
+	#p300_multi.transfer(1, reaction_plate.cols(i).bottom(0.5), agar_plate.cols(i+count2+2).bottom(-1))
 	#count2 += 2
 	p300_multi.drop_tip()
